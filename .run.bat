8b5d3380e1dca1ddfcd75ee5af682cb7bf6b9fbc
Kernie <karapys.mor@gmail.com>
2020-09-19 18:10:43 +0300

    migrations and static

--- .run.bat
+++ .run.bat
@@ -1,1 +1,1 @@
-celery worker -A app.celery --loglevel=DEBUG --concurrency=1 -P solo
+celery worker -A app.celery --loglevel=DEBUG --concurrency=1 -P gevent -E
--- start.sh
+++ start.sh
@@ -2,4 +2,5 @@
 mv env .env
 source .env
 cd app
+celery worker -A app.celery --loglevel=DEBUG --concurrency=1 -E &
 gunicorn -w 1 --bind 0.0.0.0:8080 --bind 0.0.0.0:3306 --log-level debug wsgi:app
